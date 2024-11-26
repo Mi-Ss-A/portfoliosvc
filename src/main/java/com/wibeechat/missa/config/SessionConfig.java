3e36f13556d33515eff19ecccfa82489894c6266
been980804 <dlgusqls980804@naver.com>
2024-11-26 17:44:14 +0900

    fix:redis namaspace 삭제

--- src/main/java/com/wibeechat/missa/config/SessionConfig.java
+++ src/main/java/com/wibeechat/missa/config/SessionConfig.java
@@ -10,7 +10,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableRedisHttpSession(redisNamespace = "wibee:session")
+@EnableRedisHttpSession
 public class SessionConfig {
 
     @Primary
