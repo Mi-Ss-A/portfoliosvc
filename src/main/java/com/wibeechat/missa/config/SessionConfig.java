8bb65f1b5584f1b2acaa20ff308cf60937a470e8
been980804 <dlgusqls980804@naver.com>
2024-11-26 15:19:34 +0900

    fix:usersvc와 동일한 redis namespace 부여

--- src/main/java/com/wibeechat/missa/config/SessionConfig.java
+++ src/main/java/com/wibeechat/missa/config/SessionConfig.java
@@ -10,7 +10,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableRedisHttpSession
+@EnableRedisHttpSession(redisNamespace = "wibee:session")
 public class SessionConfig {
 
     @Primary
